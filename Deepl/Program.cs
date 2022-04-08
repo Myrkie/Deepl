440226763eafb5f5f820d942371b6de6eea67f92
Myrkur <George12047518@gmail.com>
2022-04-08 13:21:28 -0400

    updated exception handling

--- Deepl/Program.cs
+++ Deepl/Program.cs
@@ -1,12 +1,21 @@
-﻿using DeeplTranslator = Deepl.Deepl; 
+﻿using System.Net;
+using DeeplTranslator = Deepl.Deepl; 
 using Language = Deepl.Deepl.Language;
 
 class Program 
 {
     static void Main(string[] args)
     {
-        var translator = new DeeplTranslator(selectedLanguage: Language.FR, targetLanguage: Language.IT, "j'adore nager même");
+        Console.WriteLine(TranslateText("【3Dモデル】Xena -ゼナ-"));
 
-        Console.WriteLine(translator.Resp);
+
+        Thread.Sleep(500000);
+    }
+    
+    private static string TranslateText(string input)
+    {
+        var translate = new DeeplTranslator(selectedLanguage: Language.JP, targetLanguage: Language.EN, input,"1231232313", new NetworkCredential("nfbnwiiu", "oa2ev6ilh71n"));
+        if (string.IsNullOrEmpty(translate.Resp)) return $"{input} \nThis Failed To translate";
+        return translate.Resp;
     }
 }
